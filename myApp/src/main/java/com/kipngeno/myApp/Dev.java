acff29db8ce561af57b58532ab529f46056c0fd3
brian070069 <kkipngeno409@gmail.com>
2025-05-26 16:46:55 +0300

    dependency injection in code

---
+++ myApp/src/main/java/com/kipngeno/myApp/Dev.java
@@ -0,0 +1,11 @@
+package com.kipngeno.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build() {
+        System.out.println("Working on Awesome Project");
+    }
+}
